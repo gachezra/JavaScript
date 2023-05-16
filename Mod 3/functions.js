5780273b919c575c096a9b7f63834d189402df39
ezragach21 <gachomoezra@gmail.com>
2023-05-16 15:07:32 +0300

    functions

---
+++ Mod 3/functions.js
@@ -0,0 +1,13 @@
+function sayHello(name){
+    console.log('----------------');
+    console.log('|              |');
+    console.log('|              |');
+    console.log('| Hii! '+ name +'    |');
+    console.log('|              |');
+    console.log('|              |');
+    console.log('----------------');
+}
+
+sayHello('Loki');
+sayHello('Thor');
+sayHello('Guy');
