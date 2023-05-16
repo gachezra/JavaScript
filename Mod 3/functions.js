ef6019be417091df1343272d3c650f7285f1f85a
ezragach21 <gachomoezra@gmail.com>
2023-05-16 16:21:28 +0300

    Much

--- Mod 3/functions.js
+++ Mod 3/functions.js
@@ -1,13 +1,24 @@
-function sayHello(name){
-    console.log('----------------');
-    console.log('|              |');
-    console.log('|              |');
-    console.log('| Hii! '+ name +'    |');
-    console.log('|              |');
-    console.log('|              |');
-    console.log('----------------');
-}
+// function sayHello(name){
+//     console.log('----------------');
+//     console.log('|              |');
+//     console.log('|              |');
+//     console.log('| Hii! '+ name +'    |');
+//     console.log('|              |');
+//     console.log('|              |');
+//     console.log('----------------');
+// }
 
-sayHello('Loki');
-sayHello('Thor');
-sayHello('Guy');
+// sayHello('Loki');
+// sayHello('Thor');
+// sayHello('Guys');
+
+// function calculateTax(amount){
+//     let result = amount * 0.25;
+//     return result;
+// }
+// let tax =  calculateTax(1982);
+// console.log(tax);
+setTimeout(
+    function(){
+        console.log('I waited 2 seconds');
+    }, 2000);
